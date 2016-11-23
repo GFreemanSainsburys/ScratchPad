c91121fdfc55c56cde539a2f4e66fbbfefeef10c
Gareth.Freeman <Gareth.Freeman@WALSNT80759>
2016-11-23 10:01:19 +0000

    Initial Project commit

---
+++ ScratchPadWeb/ScratchPadWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScratchPadWeb.MvcApplication" Language="C#" %>
---
+++ ScratchPadWeb/ScratchPadWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScratchPadWeb.Startup))]
+namespace ScratchPadWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
